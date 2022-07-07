65c6163b664a31d94fbdfc8722c811d9d00df2bb
dmccoystephenson <dmccoystephenson@gmail.com>
2022-07-06 22:33:16 -0600

    Modified the output of the help command.

--- src/main/java/dansplugins/netheraccesscontroller/commands/HelpCommand.java
+++ src/main/java/dansplugins/netheraccesscontroller/commands/HelpCommand.java
@@ -9,9 +9,9 @@ public class HelpCommand {
         sender.sendMessage(ChatColor.AQUA + "=== Nether Access Controller Commands ===");
         sender.sendMessage(ChatColor.AQUA + "/nac help - View a list of helpful commands.");
         sender.sendMessage(ChatColor.AQUA + "/nac list - List players who are allowed to access the nether.");
-        sender.sendMessage(ChatColor.AQUA + "/nac allow - Allow a player to access the nether.");
-        sender.sendMessage(ChatColor.AQUA + "/nac deny - Disallow a player from accessing the nether.");
-        sender.sendMessage(ChatColor.AQUA + "/nac config - View or set config options.");
+        sender.sendMessage(ChatColor.AQUA + "/nac allow <ign> - Allow a player to access the nether.");
+        sender.sendMessage(ChatColor.AQUA + "/nac deny <ign> - Disallow a player from accessing the nether.");
+        sender.sendMessage(ChatColor.AQUA + "/nac config <show | set> - View or set config options.");
         return true;
     }
 
